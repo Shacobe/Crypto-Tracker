8dc048317f4dff235b64a5d2cf2fe0c6c6fb54cb
Jacob Fraser <38965543+git-fraser@users.noreply.github.com>
2021-05-26 22:43:42 +1200

    Add request to return url info on BTC, ETH and XRP

--- cryptoTracker.py
+++ cryptoTracker.py
@@ -1,1 +1,4 @@
-print("Hello World!")
+# This program returns the url information from a request for BTC, ETH and XRP cryptocurrency statistics
+import urllib.request
+url = "https://api.nomics.com/v1/currencies/ticker?key=b408ad12a41f8c155a3b773178751d70f38a1509&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1"
+print(urllib.request.urlopen(url).read())
