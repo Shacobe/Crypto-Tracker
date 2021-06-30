010f9cff1f90577eb97fb0415fae99295ea48fdc
Jacob Fraser <38965543+fraserjacob@users.noreply.github.com>
2021-06-30 19:55:07 +1200

    Convert url response from bytes to string

--- cryptoTracker.py
+++ cryptoTracker.py
@@ -1,4 +1,6 @@
 # This program returns the url information from a request for BTC, ETH and XRP cryptocurrency statistics
 import urllib.request
 url = "https://api.nomics.com/v1/currencies/ticker?key=your-key-here&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1"
-print(urllib.request.urlopen(url).read())
+f = urllib.request.urlopen(url)
+url_str = (str(f.read(), 'utf-8'))
+print(url_str)
