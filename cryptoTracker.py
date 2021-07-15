38bc05f56957a3b57fc4f709f108e39dd504e333
Jacob Fraser <38965543+fraserjacob@users.noreply.github.com>
2021-07-15 17:59:51 +1200

    Prints each statistic of the requested cryptocurrency

--- cryptoTracker.py
+++ cryptoTracker.py
@@ -1,6 +1,10 @@
 # This program returns the url information from a request for BTC, ETH and XRP cryptocurrency statistics
 import urllib.request
 url = "https://api.nomics.com/v1/currencies/ticker?key=your-key-here&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1"
-f = urllib.request.urlopen(url)
-url_str = (str(f.read(), 'utf-8'))
-print(url_str)
+file = urllib.request.urlopen(url)
+url_str = (str(file.read(), 'utf-8'))
+
+# Prints each separate item in the url
+values = url_str.split(",")
+for item in values:
+    print(item)
