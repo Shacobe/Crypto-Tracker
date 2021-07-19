c459d3193874a08e574888e12c2a9ac8fc789623
Jacob Fraser <38965543+fraserjacob@users.noreply.github.com>
2021-07-20 06:20:21 +1200

    Print the price of the first currency given

--- cryptoTracker.py
+++ cryptoTracker.py
@@ -4,7 +4,23 @@ url = "https://api.nomics.com/v1/currencies/ticker?key=your-key-here&ids=BTC,ETH
 file = urllib.request.urlopen(url)
 url_str = (str(file.read(), 'utf-8'))
 
-# Prints each separate item in the url
 values = url_str.split(",")
+crypto_str1 = ""
+crypto_price_str1 = ""
+first_done_id = False
+first_done_price = False
+
+# Prints each separate item in the url
 for item in values:
     print(item)
+
+    # Gets the id and current price of the first cryptocurrency in the URL
+    if "id" in item and first_done_id is False:
+        crypto_str1 = item[7:]
+        first_done_id = not first_done_id
+    if "price" in item and "date" not in item and "time" not in item and first_done_price is False:
+        crypto_price_str1 = item[8:]
+        first_done_price = not first_done_price
+
+print("Current " + crypto_str1 + " price is:")
+print(crypto_price_str1)
