508b538d6a24b18f08f85a29fd9d10faca356786
Jacob Fraser <38965543+fraserjacob@users.noreply.github.com>
2021-11-14 17:15:21 +1300

    New API which prints out the name and price of cryptos

--- cryptoTracker.py
+++ cryptoTracker.py
@@ -1,14 +1,11 @@
-# This program returns the url information from a request for cryptocurrency statistics
+# This program returns the statistics from a request for BTC, ETH and XRP cryptocurrencies
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-# Gets the statistics of the first 5 cryptocurrencies in the latest coinmarketcap listing
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 parameters = {
-    'start': '1',
-    'limit': '5',
-    'convert': 'USD'
+    'symbol': 'BTC,ETH,XRP'
 }
 headers = {
     'Accepts': 'application/json',
@@ -22,10 +19,28 @@ try:
     response = session.get(url, params=parameters)
     data = json.loads(response.text)
 
-    # Converts json to string and prints each individual statistic
+    # Converts json to string and prints each the price for each crypto
     data_str = json.dumps(data)
     values = data_str.split(",")
+    symbol_list = []
+    price_list = []
+    num = 0
+
     for item in values:
-        print(item)
+
+        # Records the symbol and current price of the cryptocurrencies from the data
+        if "symbol" in item:
+            symbol_str = item[10:]
+            symbol_list.append(symbol_str)
+
+        if "price" in item:
+            price_str = item[19:]
+            price_list.append(price_str)
+
+    for item in symbol_list:
+        print("Current " + item + " price is:")
+        print(price_list[num] + " USD")
+        print("\n")
+        num = num + 1
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
